f480b13a4a417cb283861b977e27c19c1e9a5254
PBDott <haho020304@naver.com>
2024-10-09 17:00:23 +0900

    Removed unnecessary files and added trade DB

--- capstone-backend/src/club/dto/delete-club.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsInt, IsNotEmpty } from 'class-validator';
-
-export class DeleteClubDto {
-  @IsInt()
-  @IsNotEmpty()
-  id: number;
-}
--- capstone-backend/src/member/member.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MemberService } from './member.service';
-import { MemberController } from './member.controller';
-
-@Module({
-  controllers: [MemberController],
-  providers: [MemberService, MemberController],
-})
-export class MemberModule {}
