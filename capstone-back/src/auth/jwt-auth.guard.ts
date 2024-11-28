d82d9be656cc3f4bb14861f02e713bb5b5bf89ac
msung00 <7118sms@naver.com>
2024-11-29 03:23:13 +0900

    Feature: now guard JwtAuthGuard adds payload(userId, ...) to request object(as src/common/user.interface.ts)

--- capstone-back/src/auth/jwt-auth.guard.ts
+++ capstone-back/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
+    const request = context.switchToHttp().getRequest();
+
+    if (user) {
+      request.payload = user;
+    }
+    const userInfo = super.handleRequest(err, user, info, context, status);
+    return userInfo;
+  }
+}
