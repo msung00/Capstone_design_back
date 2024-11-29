617821e13a2d6972e2f86dfd5455214de9d7379f
msung00 <138764247+msung00@users.noreply.github.com>
2024-11-29 17:45:32 +0900

    Fix: abort unused legacy

--- capstone-back/src/chat/chat.gateway.ts
+++ capstone-back/src/chat/chat.gateway.ts
@@ -1,39 +1,39 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
-import { ChatService } from './chat.service';
-import { CreateMessageDto } from './dto/create-message.dto';
+// import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+// import { Server, Socket } from 'socket.io';
+// import { ChatService } from './chat.service';
+// import { CreateMessageDto } from './dto/create-message.dto';
 
-@WebSocketGateway({
-    namespace: '/chat',
-    cors: {
-    origin: '*',
-    },
-})
-export class ChatGateway {
-    constructor(private readonly chatService: ChatService) {}
+// @WebSocketGateway({
+//     namespace: '/chat',
+//     cors: {
+//     origin: '*',
+//     },
+// })
+// export class ChatGateway {
+//     constructor(private readonly chatService: ChatService) {}
 
-    @WebSocketServer()
-    server: Server;
+//     @WebSocketServer()
+//     server: Server;
 
-    @SubscribeMessage('sendMessage')
-    async handleMessage(
-    @MessageBody() createMessageDto: CreateMessageDto,
-    @ConnectedSocket() client: Socket,
-    ) {
-    const message = await this.chatService.sendMessage(createMessageDto);
-    this.server.to(createMessageDto.room).emit('receiveMessage', message);
-    return message;
-    }
+//     @SubscribeMessage('sendMessage')
+//     async handleMessage(
+//     @MessageBody() createMessageDto: CreateMessageDto,
+//     @ConnectedSocket() client: Socket,
+//     ) {
+//     const message = await this.chatService.sendMessage(createMessageDto);
+//     this.server.to(createMessageDto.room).emit('receiveMessage', message);
+//     return message;
+//     }
 
-    @SubscribeMessage('joinRoom')
-    handleJoinRoom(@MessageBody('room') room: string, @ConnectedSocket() client: Socket) {
-    client.join(room);
-    client.emit('joinedRoom', room);
-    }
+//     @SubscribeMessage('joinRoom')
+//     handleJoinRoom(@MessageBody('room') room: string, @ConnectedSocket() client: Socket) {
+//     client.join(room);
+//     client.emit('joinedRoom', room);
+//     }
 
-    @SubscribeMessage('leaveRoom')
-    handleLeaveRoom(@MessageBody('room') room: string, @ConnectedSocket() client: Socket) {
-    client.leave(room);
-    client.emit('leftRoom', room);
-    }
-}
+//     @SubscribeMessage('leaveRoom')
+//     handleLeaveRoom(@MessageBody('room') room: string, @ConnectedSocket() client: Socket) {
+//     client.leave(room);
+//     client.emit('leftRoom', room);
+//     }
+// }
