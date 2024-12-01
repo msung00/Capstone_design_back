32c068fe6378c55c8560e7e02047610588f3125c
msung00 <7118sms@naver.com>
2024-12-02 01:07:33 +0900

    Style: white-spacing and add error logging

--- capstone-back/src/app-response/app-response.controller.ts
+++ capstone-back/src/app-response/app-response.controller.ts
@@ -5,45 +5,47 @@ import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 @Controller('app-response')
 export class AppResponseController {
-    constructor(private readonly applicationResponseService: AppResponseService) {}
+  constructor(private readonly applicationResponseService: AppResponseService) { }
 
-    @Post('')
-    @UseGuards(JwtAuthGuard)
-    async createResponse(@Body() createDto: CreateAppResponseDto, @Req() req) {
-      try {
-        const userId = req.user.userId;
+  @Post('')
+  @UseGuards(JwtAuthGuard)
+  async createResponse(@Body() createDto: CreateAppResponseDto, @Req() req) {
+    try {
+      const userId = req.user.userId;
 
-        const appData = {...createDto, userId};
-        return await this.applicationResponseService.createResponse(appData);
-      } catch (error) {
-        console.log(error)
-        throw new InternalServerErrorException('Failed to create application response');
-      }
+      const appData = { ...createDto, userId };
+      return await this.applicationResponseService.createResponse(appData);
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Failed to create application response');
     }
+  }
 
-    @Get('byApplication')
-    async getResponsesByApplicationId(@Query('applicationId', ParseIntPipe) applicationId: number) {
-      try {
-        const responses = await this.applicationResponseService.getResponsesByApplicationId(applicationId);
-        if (!responses) {
-          throw new NotFoundException(`No responses found for application ID ${applicationId}`);
-        }
-        return responses;
-      } catch (error) {
-        throw new InternalServerErrorException('Failed to fetch responses by application ID');
+  @Get('byApplication')
+  async getResponsesByApplicationId(@Query('applicationId', ParseIntPipe) applicationId: number) {
+    try {
+      const responses = await this.applicationResponseService.getResponsesByApplicationId(applicationId);
+      if (!responses) {
+        throw new NotFoundException(`No responses found for application ID ${applicationId}`);
       }
+      return responses;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Failed to fetch responses by application ID');
     }
+  }
 
-    @Get('byUser')
-    async getResponsesByUserId(@Query('userId', ParseIntPipe) userId: number) {
-      try {
-        const responses = await this.applicationResponseService.getResponsesByUserId(userId);
-        if (!responses) {
-          throw new NotFoundException(`No responses found for user ID ${userId}`);
-        }
-        return responses;
-      } catch (error) {
-        throw new InternalServerErrorException('Failed to fetch responses by user ID');
+  @Get('byUser')
+  async getResponsesByUserId(@Query('userId', ParseIntPipe) userId: number) {
+    try {
+      const responses = await this.applicationResponseService.getResponsesByUserId(userId);
+      if (!responses) {
+        throw new NotFoundException(`No responses found for user ID ${userId}`);
       }
+      return responses;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Failed to fetch responses by user ID');
     }
+  }
 }
